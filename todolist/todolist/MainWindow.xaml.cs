a8f9baf934004496d8b4e7f6fa41363b7d7834ef
m305-9\w401 <A106082014@mail.shu.edu.tw>
2018-05-24 18:05:58 +0800

    課堂

---
+++ todolist/todolist/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todolist
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // 建立原件
+            todo item = new todo();
+
+            // 放入到 StackPanel
+            TodoItemList.Children.Add(item);
+        }
+
+        // 關閉視窗的事件
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            string data = "";
+
+            // 取得每一個項目的文字
+            foreach (todo item in TodoItemList.Children)
+            {
+                // 打勾符號
+                if (item.IsChecked == true)
+                    data += "+";
+                else
+                    data += "-";
+
+                // 文字
+                data += "|" + item.ItemName + "\r\n";
+            }
+
+            // 存檔
+            System.IO.File.WriteAllText(@"C:\temp\data.txt", data);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 讀檔
+            string[] lines = System.IO.File.ReadAllLines(@"C:\temp\data.txt");
+
+
+            // 逐行產生元件
+            foreach (string line in lines)
+            {
+                // 用符號分隔
+                string[] parts = line.Split('|');
+
+                // 建立元件
+                todo item = new todo();
+                item.ItemName = parts[1];
+
+                if (parts[0] == "+")
+                    item.IsChecked = true;
+                else
+                    item.IsChecked = false;
+
+                // 放入到 StackPanel
+                TodoItemList.Children.Add(item);
+            }
+        }
+    }
+}
---
+++ todolist/todolist/todo.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todolist
+{
+    /// <summary>
+    /// todo.xaml 的互動邏輯
+    /// </summary>
+    public partial class todo : UserControl
+    {
+        public string ItemName
+        {
+            get
+            {
+                return ItemNameTb.Text;
+            }
+            set
+            {
+                ItemNameTb.Text = value;
+            }
+        }
+
+            public bool IsChecked
+            {
+                set
+                {
+                    if (value == true)
+                    {
+                        CheckMark.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        CheckMark.Visibility = Visibility.Collapsed;
+                    }
+                }
+                get
+                {
+                    if (CheckMark.Visibility == Visibility)
+                    return true;
+                    else
+                    return false;
+                }
+        }
+
+        public todo()
+        {
+            InitializeComponent();
+        }
+
+        private void ItemNameTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (CheckMark.Visibility == Visibility)
+            {
+                CheckMark.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                CheckMark.Visibility = Visibility.Visible;
+            }
+        }
+
+
+    }
+}
